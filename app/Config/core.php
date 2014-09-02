52004f900aa84a3318bb94d56298293f08dfe3af
Matheus Peviani Vellone <matheusvellone@hotmail.com>
2014-09-02 09:17:52 -0300

    DebugKit + configurações iniciais

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'AtTFv5xx6xrLtjF7yUZ6664bg86YQHEWyUi67sfvJ');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '35653729534697963894944832396');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Application level Controller
  *
@@ -18,7 +19,6 @@
  * @since         CakePHP(tm) v 0.2.9
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-
 App::uses('Controller', 'Controller');
 
 /**
@@ -31,4 +31,9 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+
+    public $components = array(
+        'DebugKit.Toolbar'
+    );
+
 }
